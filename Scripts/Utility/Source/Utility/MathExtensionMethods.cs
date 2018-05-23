0cea0741dcdee1958672a0be7e5c2e40c511b11e
IbakonFerba <fabi.kbr97@gmail.com>
2018-05-23 10:26:07 +0200

    Added Time formatting

--- Scripts/Utility/Source/Utility/MathExtensionMethods.cs
+++ Scripts/Utility/Source/Utility/MathExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿namespace FK.Utility.MathExtensions
+﻿using UnityEngine;
+
+namespace FK.Utility.MathExtensions
 {
     /// <summary>
     /// Extensions for math operations on math types
@@ -17,6 +19,44 @@
         public static float Remap(this float value, float from1, float to1, float from2, float to2)
         {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+
+
+        /// <summary>
+        /// Formats a float as a time
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <param name="format">This string defines the formating. You can use the following keywords seperated by colons: mil - milliseconds, sec - seconds, min - minutes, hr - hours. If you want the time to tisplay minutes and seconds, this string should be "min:sec"</param>
+        /// <returns></returns>
+        public static string FormatAsTime(this float seconds, string format)
+        {
+            string[] times = format.Split(':');
+
+            string formattetTime = "";
+            for(int i = 0; i < times.Length; ++i)
+            {
+                string time = times[i];
+                switch(time)
+                {
+                    case "mil":
+                        formattetTime += Mathf.FloorToInt((seconds % 1) * 100).ToString("00");
+                        break;
+                    case "sec":
+                        formattetTime += Mathf.FloorToInt(seconds % 60).ToString("00");
+                        break;
+                    case "min":
+                        formattetTime += Mathf.FloorToInt((seconds / 60) % 60).ToString("00");
+                        break;
+                    case "hr":
+                        formattetTime += Mathf.FloorToInt((seconds / 60) / 60).ToString("00");
+                        break;
+                }
+
+                if (i < times.Length - 1)
+                    formattetTime += ":";
+            }
+
+            return formattetTime;
         }
     }
 }
