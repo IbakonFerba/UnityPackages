4d6a01043ced37aa48b443064632858faa1455e7
IbakonFerba <fabi.kbr97@gmail.com>
2018-06-15 15:16:53 +0200

    Formattet Summary

--- Scripts/FileSystem/Source/FileSystem.cs
+++ Scripts/FileSystem/Source/FileSystem.cs
@@ -43,10 +43,10 @@ namespace FK.IO
     }
 
     /// <summary>
-    /// A FileSystem that acesses the File System of the used Device and loads entries as they are needed, while saving them when they where visited so they don't have to be loaded every time
-    /// It provides easy traversal of the File Tree while only Displaying Files you want to see if you give it a list of extensions.
-    /// You can define a path you want to go to and it loads that path and gives you all entries and it provides a history of entered directories.
-    /// If needed, it can recursively reload all the Nodes that are loaded
+    /// <para>A FileSystem that acesses the File System of the used Device and loads entries as they are needed, while saving them when they where visited so they don't have to be loaded every time.</para>
+    /// <para>It provides easy traversal of the File Tree while only Displaying Files you want to see if you give it a list of extensions.</para>
+    /// <para>You can define a path you want to go to and it loads that path and gives you all entries and it provides a history of entered directories.
+    /// If needed, it can recursively reload all the Nodes that are loaded</para>
     /// 
     /// v1.1 06/2018
     /// Written by Fabian Kober
