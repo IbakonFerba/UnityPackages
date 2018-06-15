8a5c403f04254cb28a2abde8a5b48af8d2cf38ef
IbakonFerba <fabi.kbr97@gmail.com>
2018-06-15 14:36:32 +0200

    Added 3D Buttons

---
+++ Scripts/WorldInteraction/Source/3DButtons/Interactable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// base class for interactables
+/// </summary>
+public class Interactable : MonoBehaviour
+{
+    /// <summary>
+    /// If Active is false, the Button won't register any interaction
+    /// </summary>
+    public bool Active = true;
+}
