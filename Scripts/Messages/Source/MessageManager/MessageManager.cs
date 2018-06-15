fa13f8d7a8030bbf1500239fcad39c619f08919b
IbakonFerba <fabi.kbr97@gmail.com>
2018-06-15 15:17:32 +0200

    Formattet Summary

--- Scripts/Messages/Source/MessageManager/MessageManager.cs
+++ Scripts/Messages/Source/MessageManager/MessageManager.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 namespace FK.MessageManager
 {
     /// <summary>
-    /// MessageManager allows you to send Messages to any object that has registered an observer for that message type.
-    /// If you are using .NET 4.x these messages can have up to 16 parameters, else they can have up to 4.
+    ///<para>MessageManager allows you to send Messages to any object that has registered an observer for that message type.
+    /// If you are using .NET 4.x these messages can have up to 16 parameters, else they can have up to 4.</para>
     /// 
-    /// It is recommended to define your message IDs as const so they can be easily referenced and identified when you read your code.
+    /// <para>It is recommended to define your message IDs as const so they can be easily referenced and identified when you read your code.</para>
     /// 
     /// v1.0 06/2018
     /// Written by Fabian Kober
