de7aa27cdf97532f552c0c4898f9e676edb93fac
Fabian Kober <fabi.kbr97@gmail.com>
2018-08-06 14:56:15 +0200

    Added Option to flip facing in Billboard

--- Scripts/Utility/Source/Utility/Components/UI/Billboard.cs
+++ Scripts/Utility/Source/Utility/Components/UI/Billboard.cs
@@ -5,12 +5,18 @@ namespace FK.Utility.UI
     /// <summary>
     /// <para>Class to make UI face the main camera or a specified target</para>
     /// 
-    /// v1.0 06/2018
+    /// v1.1 08/2018
     /// Written by Fabian Kober
     /// fabian-kober@gmx.net
     /// </summary>
     public class Billboard : MonoBehaviour
     {
+        /// <summary>
+        /// If this is true, the forward vector will face away from the target, alse it will point at the target
+        /// </summary>
+        [Tooltip("If this is true, the forward vector will face away from the target, alse it will point at the target")]
+        public bool FlipFacing = true;
+
         /// <summary>
         /// If this is set, the UI should face it instead of the main Camera
         /// </summary>
@@ -22,7 +28,9 @@ namespace FK.Utility.UI
             Transform target = Target ? Target : Camera.main.transform;
 
             transform.LookAt(target);
-            transform.forward = -transform.forward;
+
+            if (FlipFacing)
+                transform.forward = -transform.forward;
         }
     }
 }
