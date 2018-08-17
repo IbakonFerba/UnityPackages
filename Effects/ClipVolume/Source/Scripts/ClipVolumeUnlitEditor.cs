1407b205853ffd8b870364e281fe1b667a40760b
Fabian Kober <fabi.kbr97@gmail.com>
2018-08-17 17:33:56 +0200

    added inspectors

---
+++ Effects/ClipVolume/Source/Scripts/ClipVolumeUnlitEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+/// <summary>
+/// <para>Material Editor for Unlit Clip Volume Shader</para>
+///
+/// v1.0 08/2018
+/// Written by Fabian Kober
+/// fabian-kober@gmx.net
+/// </summary>
+public class ClipVolumeUnlitEditor : ClipVolumeStandardEditor 
+{
+	// ######################## UNITY EVENT FUNCTIONS ######################## //
+	public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+	{
+		ShowGeneralProperties1(materialEditor);
+		ShowGeneralProperties2(materialEditor);  
+		ShowInsideColor(materialEditor);
+		ShowGeneralProperties3(materialEditor);
+	}
+}
