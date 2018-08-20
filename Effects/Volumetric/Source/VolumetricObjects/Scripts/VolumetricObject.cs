251c1042ed4554b44c42643592f104fcdd7ebb0a
Fabian Kober <fabi.kbr97@gmail.com>
2018-08-20 16:38:03 +0200

    Fixed Build time error

--- Effects/Volumetric/Source/VolumetricObjects/Scripts/VolumetricObject.cs
+++ Effects/Volumetric/Source/VolumetricObjects/Scripts/VolumetricObject.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// <summary>
 /// <para>This class represents a Volumetric Object. It contains all the data and manages registering itself at the VolumetricObjectRenderer</para>
 ///
-/// v2.0 08/2018
+/// v2.1 08/2018
 /// Written by Fabian Kober
 /// fabian-kober@gmx.net
 /// </summary>
@@ -179,6 +179,7 @@ public class VolumetricObject : MonoBehaviour
 
     #region EDITOR
 
+#if UNITY_EDITOR
     /// <summary>
     /// Creates a Volumetric Object
     /// </summary>
@@ -238,6 +239,7 @@ public class VolumetricObject : MonoBehaviour
     {
         VolumetricObject vo = CreateVolumetricObject(menuCommand, Types.CAPSULE, "VolumetricCapsule");
     }
+#endif
 
     #endregion
 }
