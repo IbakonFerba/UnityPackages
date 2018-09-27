9e706d56ca18353a7a12481ec261be9eec4760ac
Fabian Kober <fabi.kbr97@gmail.com>
2018-09-27 12:58:37 +0200

    Added Editor Source

---
+++ Effects/MagicLight/Source/MagicLightUnlitEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+/// <summary>
+/// <para>Material Editor for Unlit Magic Light Shader</para>
+///
+/// v1.0 09/2018
+/// Written by Fabian Kober
+/// fabian-kober@gmx.net
+/// </summary>
+public class MagicLightUnlitEditor : MagicLightStandardEditor 
+{
+	// ######################## UNITY EVENT FUNCTIONS ######################## //	
+	public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+	{
+		ShowGeneralProperties1(materialEditor);
+		ShowGeneralProperties2(materialEditor);
+		ShowGeneralProperties3(materialEditor, false);
+		ShowAdvancedOptions(materialEditor);
+	}
+}
