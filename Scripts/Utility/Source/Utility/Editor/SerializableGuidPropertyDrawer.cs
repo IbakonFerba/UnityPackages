6bc8d2141deed47117322eaa96579c2c8b7aec26
Fabian Kober <fabian-kober@gmx.net>
2019-10-18 15:18:37 +0200

    + Utility: Serializable Guid

---
+++ Scripts/Utility/Source/Utility/Editor/SerializableGuidPropertyDrawer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace FK.Utility.Editor
+{
+    /// <summary>
+    /// <para>A custom property drawer for the serializable guid. This uses IMGUI because the default inspector does not yet support UIElements</para>
+    ///
+    /// v1.0 10/2019
+    /// Written by Fabian Kober
+    /// fabian-kober@gmx.net
+    /// </summary>
+    [CustomPropertyDrawer(typeof(SerializableGuid))]
+    public class SerializableGuidPropertyDrawer : PropertyDrawer
+    {
+        // ######################## UNITY EVENT FUNCTIONS ######################## //
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // Using BeginProperty / EndProperty on the parent property means that
+            // prefab override logic works on the entire property.
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            // Draw fields
+            EditorGUI.LabelField(position, property.FindPropertyRelative("_serializedGuid").stringValue);
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Scripts/Utility/Source/Utility/SerializableGuid.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using UnityEngine;
+
+namespace FK.Utility
+{
+    /// <summary>
+    /// <para>A serializable system.Guid</para>
+    ///
+    /// v1.0 10/2019
+    /// Written by Fabian Kober
+    /// fabian-kober@gmx.net
+    /// </summary>
+    [System.Serializable]
+    public struct SerializableGuid : ISerializationCallbackReceiver
+    {
+        // ######################## PROPERTIES ######################## //
+        /// <summary>
+        /// The actual System.Guid behind this
+        /// </summary>
+        public Guid Value => _guid;
+
+
+        // ######################## PRIVATE VARS ######################## //
+        [SerializeField] private string _serializedGuid;
+        private Guid _guid;
+
+
+        // ######################## OPERATORS ######################## //
+        #region Operators
+
+        public static implicit operator SerializableGuid(Guid value)
+        {
+            return new SerializableGuid(value);
+        }
+
+        public static bool operator ==(SerializableGuid a, SerializableGuid b)
+        {
+            return a.Value == b.Value;
+        }
+
+        public static bool operator ==(SerializableGuid a, Guid b)
+        {
+            return a.Value == b;
+        }
+
+        public static bool operator ==(Guid a, SerializableGuid b)
+        {
+            return a == b.Value;
+        }
+
+        public static bool operator !=(SerializableGuid a, SerializableGuid b)
+        {
+            return a.Value != b.Value;
+        }
+
+        public static bool operator !=(SerializableGuid a, Guid b)
+        {
+            return a.Value != b;
+        }
+
+        public static bool operator !=(Guid a, SerializableGuid b)
+        {
+            return a != b.Value;
+        }
+
+        #endregion
+
+
+        // ######################## UNITY EVENT FUNCTIONS ######################## //
+        public void OnBeforeSerialize()
+        {
+            _serializedGuid = _guid.ToString();
+        }
+
+        public void OnAfterDeserialize()
+        {
+            _guid = Guid.Parse(_serializedGuid);
+        }
+
+
+        // ######################## INITS ######################## //
+        public SerializableGuid(Guid value)
+        {
+            _serializedGuid = "";
+            _guid = value;
+        }
+
+
+        // ######################## FUNCTIONALITY ######################## //
+        public int CompareTo(Guid value)
+        {
+            return Value.CompareTo(value);
+        }
+
+        public int CompareTo(SerializableGuid value)
+        {
+            return Value.CompareTo(value.Value);
+        }
+
+        public int CompareTo(System.Object value)
+        {
+            return Value.CompareTo(value);
+        }
+
+        public bool Equals(Guid value)
+        {
+            return Value.Equals(value);
+        }
+
+        public bool Equals(SerializableGuid value)
+        {
+            return Value.Equals(value.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
