f97d0f77c439a2e53167943aa01e7f6c1481f97a
Fabian Kober <fabian-kober@gmx.net>
2019-12-10 16:40:12 +0100

    + Utility: functions to get HSL form a color

--- Scripts/Utility/Source/Utility/ColorUtility.cs
+++ Scripts/Utility/Source/Utility/ColorUtility.cs
@@ -5,7 +5,7 @@ namespace FK.Utility
 	/// <summary>
 	/// <para>Utility functions for colors</para>
 	///
-	/// v1.0 12/2019
+	/// v1.1 12/2019
 	/// Written by Fabian Kober
 	/// fabian-kober@gmx.net
 	/// </summary>
@@ -31,7 +31,7 @@ namespace FK.Utility
 		/// <returns></returns>
 		public static Color HSL(float h, float s, float l, float a = 1)
 		{
-			return new Color(HSLtoRGBValueConversion(0, h, s, l), HSLtoRGBValueConversion(8, h, s, l), HSLtoRGBValueConversion(4, h, s, l), 1);
+			return new Color(HSLtoRGBValueConversion(0, h, s, l), HSLtoRGBValueConversion(8, h, s, l), HSLtoRGBValueConversion(4, h, s, l), a);
 		}
 
 		/// <summary>
@@ -58,6 +58,61 @@ namespace FK.Utility
 		public static Color RGB(int r, int g, int b, int a = 255)
 		{
 			return new Color(r/255f, g/255f, b/255f, a/255f);
+		}
+
+		/// <summary>
+		/// Returns the hue of a color between 0 and 360
+		/// </summary>
+		/// <param name="color"></param>
+		/// <returns></returns>
+		public static int Hue(this Color color)
+		{
+			float cMin = Mathf.Min(color.r, color.g, color.b);
+			float cMax = Mathf.Max(color.r, color.g, color.b);
+			float delta = cMax - cMin;
+
+			float hue = 0;
+			if (delta == 0f)
+				hue = 0;
+			else if (cMax == color.r)
+				hue = ((color.g - color.b) / delta) % 6;
+			else if (cMax == color.g)
+				hue = (color.b - color.r) / delta + 2;
+			else
+				hue = (color.r - color.g) / delta + 4;
+
+			hue = Mathf.RoundToInt(hue * 60f);
+			if (hue < 0)
+				hue += 360;
+
+			return (int)hue;
+		}
+		
+		/// <summary>
+		/// Returns the HSL saturation of a color between 0 and 100
+		/// </summary>
+		/// <param name="color"></param>
+		/// <returns></returns>
+		public static int Saturation(this Color color)
+		{
+			float cMin = Mathf.Min(color.r, color.g, color.b);
+			float cMax = Mathf.Max(color.r, color.g, color.b);
+			float delta = cMax - cMin;
+			
+			return Mathf.RoundToInt((delta == 0 ? 0 : delta / (1 - Mathf.Abs(2f * (color.Luminance()/100f) - 1))) * 100);
+		}
+
+		/// <summary>
+		/// Returns the HSL luminance of a color between 0 and 100
+		/// </summary>
+		/// <param name="color"></param>
+		/// <returns></returns>
+		public static int Luminance(this Color color)
+		{
+			float cMin = Mathf.Min(color.r, color.g, color.b);
+			float cMax = Mathf.Max(color.r, color.g, color.b);
+
+			return Mathf.RoundToInt(((cMax + cMin) / 2) * 100);
 		}
 	}
 }
